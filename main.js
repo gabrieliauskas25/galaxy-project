5c3ad501723ab1d7dac83361f64bccd9779cdae7
gabrieliauskas25 <gabrieliusmhm25@gmail.com>
2025-10-27 00:48:04 +0200

    Change THREE import to use Skypack CDN
    Updated import statement for THREE from Skypack CDN.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'https://cdn.skypack.dev/three';
 
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x000010)
